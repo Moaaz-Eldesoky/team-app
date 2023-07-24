51a86bb84c07f40b79dd28386c848f010a0b6adf
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2023-07-24 15:32:50 +0300

    first step on project add app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-app';
+  newMemberName = '';
+  members:string[] = [];
+    addMember() {
+      this.members.push(this.newMemberName);
+      console.log(this.members);
+  }
+
 }
