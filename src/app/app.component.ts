71e74767c78255a8e174898053f1c3798a3c286e
“m84288955” <moaaz.salah.eldesoky1@huawei-partners.com>
2023-07-27 17:09:42 +0300

    complete the whole functionality of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,12 +6,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-app';
-
   newMemberName = '';
   members: string[] = [];
-
   errorMessage: string = ""
-  teams = "";
+  teams = 0;
+  teamNum:number = 0;
+  newArray:string[][] = [];
+
+  // randomTeams:any[] = [];
+
   onInput(member: string) {
     this.newMemberName = member;
     console.log(this.members);
@@ -22,16 +24,45 @@ export class AppComponent {
       this.errorMessage= "name can't be empty";
     }
     else {
+      this.errorMessage="";
       this.members.push(this.newMemberName);
       this.newMemberName = "";
     }
   }
 
-  getTeams(numT: string) {
+  getTeams(numT:any) {
     this.teams = numT;
   }
-  displayTeams() {
-    console.log("pressed");
+  generateTeams() {
+    // logic of the teams generation
+    if(this.members.length%this.teams == 0){
+      this.teamNum  = this.members.length/this.teams;
+      const allMembers = [...this.members]
+      this.newArray = this.assignTeams(allMembers,this.teams);
+      this.errorMessage="";
+      console.log(this.newArray);
+    }
+    else{
+      this.errorMessage="members%teams != 0 so we can't generate teams";
+    }
   }
 
+  // chat GPT code
+  assignTeams(names: string[], numTeams: number): string[][] {
+    // Shuffle the array of names randomly
+    const shuffledNames = names.sort(() => Math.random() - 0.5);
+
+    // Calculate the number of players per team
+    const playersPerTeam = Math.floor(names.length / numTeams);
+
+    // Create an array of empty arrays for each team
+    const teams: string[][] = Array.from({ length: numTeams }, () => []);
+
+    // Assign players to teams
+    shuffledNames.forEach((name, index) => {
+      const teamIndex = Math.floor(index / playersPerTeam);
+      teams[teamIndex].push(name);
+    });
+    return teams;
+  }
 }
